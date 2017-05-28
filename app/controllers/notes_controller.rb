77fb6e655dbefab0c3707ea47a93b96d2d3f6bdc
waywho <walzerfan@yahoo.com>
2017-05-28 04:29:31 +0000

    add create action and tests functionality

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,4 +2,16 @@ class NotesController < ApplicationController
     
     def index
     end
+
+    def create
+        note = Note.create(note_params)
+
+        render json: note, status: :created
+    end
+
+    private
+
+    def note_params
+        params.require(:note).permit(:title, :content)
+    end
 end
--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -7,4 +7,28 @@ RSpec.describe NotesController, type: :controller do
             expect(response).to have_http_status(:success)
         end
     end
+
+    describe "notes#create action" do
+        before do
+            post :create, params: { note: { title: 'First', content: 'Hello'} }
+        end
+
+        it "should return 200 status-code" do
+            expect(response).to be_success
+        end
+
+        it "should successfully create and save a new note in the database" do
+            note = Note.last
+
+            expect(note.content).to eq("Hello")
+            expect(note.title).to eq("First")
+        end
+
+        it "should return the created note in response body" do
+            json = JSON.parse(response.body)
+
+            expect(json['content']).to eq("Hello")
+            expect(json['title']).to eq("First")
+        end
+    end
 end
