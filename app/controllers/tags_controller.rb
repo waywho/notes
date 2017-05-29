3392299d1d78d0ddea7aab2e823b5643f0cd81cf
waywho <walzerfan@yahoo.com>
2017-05-29 21:20:41 +0000

    add tag validation tests and handling

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,12 +2,20 @@ class TagsController < ApplicationController
 
     def create
         note = Note.find(params[:note_id])
-        note.tags.create(tag_params)
+        tag = note.tags.create(tag_params)
+
+        if tag.invalid?
+            render json: render_errors(tag), status: :unprocessable_entity
+        end
     end
 
     private
 
     def tag_params
         params.require(:tag).permit(:name)
+    end
+
+    def render_errors(tag)
+        { errors: tag.errors }
     end
 end
--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -15,4 +15,20 @@ RSpec.describe TagsController, type: :controller do
             expect(@note.tags.first.name).to eq('Crazy')
         end
     end
+
+    describe "tag#create action validations" do
+        before do
+            note = FactoryGirl.create(:note)
+            post :create, params: { tag: { name: ''}, note_id: note.id }
+        end
+
+        it "should properly deal with validation errors" do
+            expect(response).to have_http_status(:unprocessable_entity)
+        end
+
+        it "should return errors json on validations" do
+            json = JSON.parse(response.body)
+            expect(json['errors']['name'][0]).to eq("can't be blank") 
+        end
+    end
 end
